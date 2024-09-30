80827b720e15b7727342598aa3636ab59a4a2345
neocronos666 <ncpp.sys@gmail.com>
2024-09-30 19:15:24 -0300

    Implementada SearchBar | funcion de busqueda funciona

--- components/menu_find_nav.py
+++ components/menu_find_nav.py
@@ -1,5 +1,3 @@
-#Codigo de sergio
-
 import flet as ft
 from components.global_cfg import GlobalCfg
 from buffer_manager import BufferManager
@@ -7,6 +5,7 @@ from buffer_manager import BufferManager
 import subprocess
 import sys
 
+'''
 class MenuFindNav(ft.Row):
     def __init__(self,back_link):
         super().__init__()
@@ -44,15 +43,55 @@ class MenuFindNav(ft.Row):
             vertical_alignment=ft.CrossAxisAlignment.CENTER,
             expand=True
         )
-    
-    def on_settings_click(self, e): #----------------ACA------------
-       
-        # self.page.go("/settings") 
-        # print("Settings------------")
+'''
+class MenuFindNav(ft.Row):
+    def __init__(self, back_link):
+        super().__init__()
+        self.back_link = back_link
+        buffer_manager = BufferManager()
+        self.all_list = buffer_manager.get_buffer()
+        self.search_results = []
 
-        subprocess.Popen([sys.executable, 'settings_manager.py'])
-        #sys.exit()
-        # self.page.window.visible = False
+    def build(self):
+        menu = ft.PopupMenuButton(
+            items=[
+                ft.PopupMenuItem(text="Settings", on_click=self.on_settings_click),
+                ft.PopupMenuItem(text="New (soon)", on_click=self.on_new_click),
+                ft.PopupMenuItem(text="Help", on_click=self.on_help_click),
+                ft.PopupMenuItem(text="About", on_click=self.on_about_click)
+            ]
+        )
+
+        # self.search_box = ft.TextField(hint_text="Search...", expand=True, on_change=self.on_search_change)
+        self.find_button = ft.IconButton(icon=ft.icons.SEARCH, on_click=self.on_search_click)
+        self.back_button = ft.IconButton(icon=ft.icons.ARROW_BACK, on_click=self.on_back_click)
+        self.search_bar = ft.SearchBar(
+            view_elevation=4,
+            divider_color=ft.colors.AMBER,
+            bar_hint_text="Search colors...",
+            view_hint_text="Type a subject",
+            on_change=self.on_search_change,
+            on_submit=self.on_search_click,
+            on_tap=self.on_search_click
+            )
+
+        return ft.Row(
+            [
+                menu,
+                # self.search_box,
+                self.search_bar,
+                self.find_button,
+                self.back_button
+                
+            ],
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            expand=True
+        )
+    
+
+    def on_settings_click(self, e):        
+        subprocess.Popen([sys.executable, 'settings_manager.py'])        
         self.page.window.close()
         self.page.update()  
         
@@ -70,21 +109,67 @@ class MenuFindNav(ft.Row):
         g_c = GlobalCfg()
         link=g_c.go_back()        
         if link: self.page.go(link)         
-
+    '''
     def on_search_click(self, e):
         print("SEARCH------------")
         if self.search_results:
             first_result = self.search_results[0]
             self.page.go(f"sheet?s={first_result}")
+    '''
+    def on_search_click(self, e):
+        if self.search_results:
+            print("---------------RESULTADO:")
+            print(self.search_results[0])
 
-    def on_search_change(self,e):
-        # print(e.data)
-        # print(self.all_paths)
+
+
+
+    '''
+    def on_search_change(self,e):        
         query = e.data
         if len(query) > 1:
             self.search_results = [item for item in self.all_list if query.lower() in item.lower()][:10]
             self.update_dropdown()
-    
+    -----
+    def on_search_change(self, e):
+        query = self.search_bar.value
+        if len(query) >= 1: #Poner a 3 para ahorrar recursos
+            self.search_results = [item for item in self.all_list if query.lower() in item.lower()][:10]
+            #self.search_bar.controls = [ft.SearchBar(value=result) for result in self.search_results]
+            if len (self.search_results) >= 1 :
+                # self.search_bar.controls = self.get_results(self.search_results)
+                self.search_bar.controls.clear
+                self.search_bar.controls.append([         
+                        ft.ListTile(title=ft.Text(self.search_results), on_click=self.on_search_click, data=i)
+                        for i in range(len(self.search_results))
+                        ]
+                    )
+                self.search_bar.update()
+    '''
+    def on_search_change(self, e):
+        query = self.search_bar.value
+        if len(query) >= 3:  # Cambiado a 3 para ahorrar recursos
+            self.search_results = [item for item in self.all_list if query.lower() in item.lower()][:10]
+            if len(self.search_results) >= 1:
+                self.search_bar.controls.clear()  # Asegúrate de llamar al método clear() correctamente
+                self.search_bar.controls = [
+                        ft.ListTile(title=ft.Text(result), on_click=self.on_search_click, data=result)
+                        for result in self.search_results
+                    ]
+                
+                self.search_bar.update()
+  
+
+
+    def get_results(self,e,search_results):
+        controls = [
+            #ft.ListTile(title=ft.Text(f"Color {i}"), on_click=close_anchor, data=i)
+            #for i in range(10)
+            ft.ListTile(title=ft.Text(search_results), on_click=self.on_search_click, data=i)
+                for i in len(search_results)
+        ]
+        return controls
+    '''
     def update_dropdown(self):
         self.dropdown.items = [ft.DropdownItem(text=result, on_click=self.on_dropdown_click) for result in self.search_results]
         self.dropdown.update()
@@ -92,3 +177,4 @@ class MenuFindNav(ft.Row):
     def on_dropdown_click(self, e):
         selected_result = e.control.text
         self.page.go(f"sheet?s={selected_result}")
+    '''
--- screens/main_content.py
+++ screens/main_content.py
@@ -13,25 +13,6 @@ class MainContent(ft.Column):
             self.build_directory_view(),
             self.build_file_view()
         ]
-    '''    
-    def load_directory_structure(self):
-        directory_structure = {}
-        with open(self.buffer_file, "r") as f:
-            for line in f:
-                parts = line.strip().split('/')
-                current_level = directory_structure
-
-                # Guardar la línea completa en la variable de la clase
-                self.all_paths.append(line.strip())
-                
-                # Ignorar el último elemento ya que es el nombre del archivo
-                for part in parts[:-1]:  
-                    if part not in current_level:
-                        current_level[part] = {}
-                    current_level = current_level[part]
-                    
-        return directory_structure
-    '''
     def load_directory_structure(self):
         directory_structure = {}
         
@@ -56,59 +37,22 @@ class MainContent(ft.Column):
                     # Incrementamos el contador de archivos en el directorio actual
                     current_level["__file_count__"] += 1
 
-        return directory_structure    # !!!!!!!!!!!!!!!!ACA ESTA EL PROBLEMA; SE CAMBIO ESTA ESTRUCTURA!!!!!!!!!!!!!!!!!!!!!
-
-
-
-
+        return directory_structure        
     def find_full_path(self, filename):
         for path in self.all_paths:
             if path.endswith(filename):
                 return path
-        return None
-    '''
-    def build_directory_view(self, structure=None, level=0):
-        if structure is None:
-            structure = self.directory_structure
-
-        directory_view = ft.Column()
-        for directory, subdirectories in structure.items():
-            directory_view.controls.append(
-                ft.Container(
-                    padding=ft.Padding(left=10 * level, top=5, right=0, bottom=5),
-                    content=ft.Row(
-                        controls=[
-                            ft.Icon(ft.icons.FOLDER),
-                            ft.Text(directory)
-                        ],
-                    ),
-                    # Usa functools.partial para pasar el directorio como parámetro
-                    on_click=partial(self.on_category_click, directory=directory)
-                )
-            )
-            directory_view.controls.append(
-                self.build_directory_view(subdirectories, level + 1)
-            )
-        return directory_view
-    '''
+        return None   
     def build_directory_view(self, structure=None, level=0):
         if structure is None:
             structure = self.directory_structure
 
         directory_view = ft.Column()
         if isinstance(structure, dict):  # Verificamos si structure es un diccionario
-            for directory, subdirectories in structure.items():
-                # Cuenta la cantidad de archivos en la subcategoría
-                # file_count = self.count_files_in_directory(subdirectories)
-                # file_count = self.directory_structure.get("__file_count__", 0)
+            for directory, subdirectories in structure.items():               
                 if directory == "__file_count__":
-                    continue  # Omitimos el directorio __file_count__
-                
+                    continue  # Omitimos el directorio __file_count__                
                 file_count = 0  # Inicializamos file_count en 0
-
-
-
-
                 if isinstance(subdirectories, dict):
                     # Cuenta la cantidad de archivos en la subcategoría
                     file_count = int(subdirectories.get("__file_count__", 0))
@@ -137,11 +81,7 @@ class MainContent(ft.Column):
                 directory_view.controls.append(
                     self.build_directory_view(subdirectories, level + 1)
                 )
-        return directory_view
-    
-
-
-    
+        return directory_view    
     def build_file_view(self):
         file_view = ft.Column()
         with open(self.buffer_file, "r") as f:
@@ -167,22 +107,17 @@ class MainContent(ft.Column):
     def on_file_click(self, e, file_name):
         # Usar el nombre de archivo pasado como parámetro
         self.selected = self.find_full_path(file_name)
-        print("-----SELF.SELECTED:" + self.selected)
-
         self.page.go("/sheet?s=" + self.selected)
 
     def on_category_click(self, e, directory):        
-        selected_category = directory  # Obtener el valor de directory pasado por partial
-        
-        filtered_files = []
-        
+        selected_category = directory  # Obtener el valor de directory pasado por partial        
+        filtered_files = []        
         # Filtrar los archivos según la categoría seleccionada
         with open(self.buffer_file, "r") as f:
             for line in f:
                 parts = line.strip().split('/')
                 if directory in parts[:-1]:
-                    filtered_files.append(parts[-1])
-        
+                    filtered_files.append(parts[-1])        
         # Actualiza el file_view con los archivos filtrados
         file_view = ft.Column()
         for file_name in filtered_files:
@@ -206,157 +141,8 @@ class MainContent(ft.Column):
     # Función para contar los archivos en una categoría
     def count_files_in_directory(self, subdirectories):
         count = 0
-        if len(subdirectories):
-            '''
-            for subdir, contents in subdirectories.items():
-                # Si no hay subdirectorios, cuenta como archivo
-                if not contents:
-                    count += 1
-                else:
-                    count += self.count_files_in_directory(contents)
-            '''
+        if len(subdirectories):            
             for subdir, contents in subdirectories.items():
                 if not isinstance(contents, dict):        
                     count += 1
         return count
-'''
-import flet as ft
-
-class MainContent(ft.Column):
-    def __init__(self, buffer_file=None):
-        super().__init__()       
-        
-        self.buffer_file = buffer_file
-        self.all_paths = []  # Lista para almacenar las rutas completas
-        self.directory_structure = self.load_directory_structure()
-        self.controls = [
-            self.build_directory_view(),
-            self.build_file_view()
-        ]
-        
-    def load_directory_structure(self):
-        directory_structure = {}
-        with open(self.buffer_file, "r") as f:
-            for line in f:
-                parts = line.strip().split('/')
-                current_level = directory_structure
-
-                # Guardar la línea completa en la variable de la clase
-                self.all_paths.append(line.strip())
-                
-                # Ignorar el último elemento ya que es el nombre del archivo
-                for part in parts[:-1]:  
-                    if part not in current_level:
-                        current_level[part] = {}
-                    current_level = current_level[part]
-                    
-        return directory_structure
-    
-    def find_full_path(self, filename):
-        for path in self.all_paths:
-            if path.endswith(filename):
-                return path
-        return None
-
-
-    def build_directory_view(self, structure=None, level=0):
-        if structure is None:
-            structure = self.directory_structure
-
-        directory_view = ft.Column()
-        for directory, subdirectories in structure.items():
-            directory_view.controls.append(
-                ft.Container(
-                    padding=ft.Padding(left=10 * level, top=5, right=0, bottom=5),
-                    content=ft.Row(
-                        controls=[
-                            ft.Icon(ft.icons.FOLDER),
-                            ft.Text(directory)
-                        ],
-                    ),                    
-                    # on_click=lambda e: self.on_category_click
-                    on_click=self.on_category_click
-                )
-            )
-            directory_view.controls.append(
-                self.build_directory_view(subdirectories, level + 1)
-            )
-        return directory_view
-
-    def build_file_view(self):
-        file_view = ft.Column()
-        with open(self.buffer_file, "r") as f:
-            for line in f:
-                parts = line.strip().split('/')
-                file_name = parts[-1]
-                if len(parts) > 2:  # Exclude root-level directories
-                    file_view.controls.append(
-                        ft.Container(
-                            padding=ft.Padding(left=10, top=5, right=0, bottom=5),
-                            content=ft.Row(
-                                controls=[
-                                    ft.Icon(ft.icons.INSERT_DRIVE_FILE),
-                                    ft.Text(file_name)
-                                ],                                                            
-                            ),
-                            # on_click=lambda e: self.on_file_click
-                            on_click=self.on_file_click
-                        )
-                    )
-        return file_view
-
-    def on_file_click(self, e):
-        
-        selected_value = e.control.content.controls[1].value
-        # if not selected_value: selected_value ="/"
-        
-        self.selected = self.find_full_path(selected_value) #problema aca
-        
-        print("-----SELF.SELECTED:" + self.selected)
-
-        self.page.go("/sheet?s=" + self.selected)
-
-       
-
-    def on_category_click(self, e): # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!ROTO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!        
-        # Obtiene la categoría seleccionada
-        selected_category = e.control.content.controls[1].value
-        
-        # Inicializa una lista para almacenar los archivos correspondientes a la categoría seleccionada
-        filtered_files = []
-        
-        # Recorre el archivo buffer y filtra los archivos que pertenecen a la categoría seleccionada
-        with open(self.buffer_file, "r") as f:
-            for line in f:
-                parts = line.strip().split('/')
-                
-                # Verifica si la ruta comienza con la categoría seleccionada
-                if selected_category in parts:
-                    # Si la categoría seleccionada es la raíz, muestra todos los archivos bajo ella
-                    if parts[:-1] == selected_category.split('/'):
-                        filtered_files.append(parts[-1])
-                    # Si la categoría seleccionada está en una subcarpeta, muestra solo los archivos correspondientes
-                    elif selected_category == parts[len(parts) - len(parts):len(parts)]:
-                        filtered_files.append(parts[-1])
-        
-        # Actualiza el file_view con los archivos filtrados
-        file_view = ft.Column()
-        for file_name in filtered_files:
-            file_view.controls.append(
-                ft.Container(
-                    padding=ft.Padding(left=10, top=5, right=0, bottom=5),
-                    content=ft.Row(
-                        controls=[
-                            ft.Icon(ft.icons.INSERT_DRIVE_FILE),
-                            ft.Text(file_name)
-                        ],
-                    ),
-                    on_click=self.on_file_click
-                )
-            )
-        
-        # Actualiza los controles en la interfaz
-        self.controls[1] = file_view
-        self.update()
-
-'''
